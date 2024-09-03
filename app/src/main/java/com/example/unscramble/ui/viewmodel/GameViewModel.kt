11960f5da07e45966ee676110b314df2e0a81fb8
Jeetin9410 <kumaranand8126@gmail.com>
2024-09-03 12:01:27 +0530

    feat: display random scrambled word logic

--- app/src/main/java/com/example/unscramble/ui/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/viewmodel/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.unscramble.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
 import com.example.unscramble.ui.model.GameStateUi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -42,4 +43,36 @@ class GameViewModel : ViewModel() {
 
      */
 
+    init {
+        resetGame()
+    }
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    private fun pickRandomWordAndShuffle(): String {
+        // Continue picking up a new random word until you get one that hasn't been used before
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameStateUi(currentScrambledWord = pickRandomWordAndShuffle())
+    }
 }
